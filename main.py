64d1c8b7017760879d94897d1b728d2d73605852
Alexis Paques <alexis.paques@gmail.com>
2021-06-24 21:39:26 +0200

    Add memory

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 import discord
 import openai
+from collections import defaultdict
 
 openai.api_key = os.environ.get("OPENAI_KEY")
 DISCORD_BOT_TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
@@ -10,6 +11,39 @@ COMMAND_KIRBY="Kirby god: "
 COMMAND_CURIE="Curie: "
 COMMAND_BABBAGE="Babbage: "
 COMMAND_ADA="Ada: "
+COMMAND_NEED_MORE="I NEED MORE"
+
+MEMORY_LIMIT = 5
+
+class OpenAIPromptResponse:
+    def __init__(self, prompt, openai_response_choice):
+        self.prompt = prompt
+        self.resp = openai_response_choice.strip()
+    def __str__(self):
+        return "".join(["\nQ: ", self.prompt, "\nA: ", self.resp, "\n"])
+
+class OpenAIMemory:
+    BASE_TEXT="Kirby, god of all beings. I anwer faithfully to all questions from my subjects.\n"
+    BASE_PROMPT=OpenAIPromptResponse("Who is god?","God is myself and myself is god.")
+    def __init__(self):
+        self.req_resps = []
+    def update(self, prompt, openai_response_choice):
+        self.req_resps.append(OpenAIPromptResponse(prompt, openai_response_choice))
+        if len(self.req_resps) > MEMORY_LIMIT:
+            self.req_resps.pop(0) 
+    def clear(self):
+        self.req_resps = []
+    def get(self):
+        result = "".join([self.BASE_TEXT])
+        if len(self.req_resps) == 0:
+            result += str(self.BASE_PROMPT)
+        else:
+            for val in self.req_resps:
+                result += str(val)
+        return result
+
+
+last_openai_request = defaultdict(OpenAIMemory)
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -20,27 +54,29 @@ class MyClient(discord.Client):
             return
         
         data = message.content
+        response = 0
+        prompt = ""
+        
         if data.startswith(COMMAND_KIRBY):
-            data = data[len(COMMAND_KIRBY):]
+            prompt = data[len(COMMAND_KIRBY):]
+            openai_prompt = "{0}\nQ: {1}\nA:".format(last_openai_request[message.author].get(), prompt)
+            print('Prompt: {0}'.format(openai_prompt))
             response = openai.Completion.create(
                 engine="curie-instruct-beta",
-                prompt="Kirby, god of all beings. I anwer faithfully to all questions from my subjects.\n\nQ: Who is god?{0}A: God is myself and myself is god.\n\nQ:{0}\nA:".format(data),
+                prompt=openai_prompt,
                 temperature=0.9,
                 max_tokens=60,
                 top_p=1.0,
                 frequency_penalty=0.3,
                 presence_penalty=0.3,
-                stop=["Q:"]
+                stop=["Q:", "A:"]
             )
-            print(response)
-            for choice in response.choices:
-                await message.channel.send('{0.text}'.format(choice))
                 
         if data.startswith(COMMAND_CURIE):
-            data = data[len(COMMAND_CURIE):]
+            prompt = data[len(COMMAND_CURIE):]
             response = openai.Completion.create(
                 engine="curie",
-                prompt="{0}\n".format(data),
+                prompt="{0}\n".format(prompt),
                 temperature=0.9,
                 max_tokens=60,
                 top_p=1.0,
@@ -48,15 +84,12 @@ class MyClient(discord.Client):
                 presence_penalty=0.3,
                 stop=["\n\n"]
             )
-            print(response)
-            for choice in response.choices:
-                await message.channel.send('{0.text}'.format(choice))
                 
         if data.startswith(COMMAND_ADA):
-            data = data[len(COMMAND_ADA):]
+            prompt = data[len(COMMAND_ADA):]
             response = openai.Completion.create(
                 engine="babbage",
-                prompt="{0}\n".format(data),
+                prompt="{0}\n".format(prompt),
                 temperature=0.9,
                 max_tokens=60,
                 top_p=1.0,
@@ -64,15 +97,12 @@ class MyClient(discord.Client):
                 presence_penalty=0.3,
                 stop=["\n\n"]
             )
-            print(response)
-            for choice in response.choices:
-                await message.channel.send('{0.text}'.format(choice))
 
         if data.startswith(COMMAND_BABBAGE):
-            data = data[len(COMMAND_BABBAGE):]
+            prompt = data[len(COMMAND_BABBAGE):]
             response = openai.Completion.create(
                 engine="ada",
-                prompt="{0}\n".format(data),
+                prompt="{0}\n".format(prompt),
                 temperature=0.9,
                 max_tokens=60,
                 top_p=1.0,
@@ -80,9 +110,12 @@ class MyClient(discord.Client):
                 presence_penalty=0.3,
                 stop=["\n\n"]
             )
-            print(response)
+
+        if response != 0:
             for choice in response.choices:
+                last_openai_request[message.author].update(prompt, choice.text)
                 await message.channel.send('{0.text}'.format(choice))
+                # "id": "cmpl-3DyYkNkjnyBSHFbSNgh03GFjI9EpC", 
         print('Message from {0.author}: {0.content}'.format(message))
 
 intents = discord.Intents.default()
